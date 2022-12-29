63f33f0979f3fc6b514201284335be59f50e5934
Geczy <Geczy@users.noreply.github.com>
2022-12-29 14:26:09 -0500

    rm twitch requirement

--- src/module.js
+++ src/module.js
@@ -1,61 +1,49 @@
 import BTTVProvider from "./providers/BTTVProvider";
 import FFZProvider from "./providers/FFZProvider";
-import TwitchProvider from "./providers/TwitchProvider";
 import SevenTVProvider from "./providers/STVProvider";
 
 class TwitchFetcher {
-    constructor (config = {}) {
+    constructor(config = {}) {
         this.bttvProvider = new BTTVProvider();
         this.ffzProvider = new FFZProvider();
         this.stvProvider = new SevenTVProvider();
-        this.twitchProvider = new TwitchProvider(config.twitchClientID, config.twitchOAuth);
     }
 
-    async getUserData ({id, username}) {
-        if (username == null && id == null) {
-            throw new Error("You must specify an ID or Username.");
-        }
-
-        if (username) {
-            return this.twitchProvider.getUserByName(username);
-        } else {
-            return this.twitchProvider.getUserByID(id);
-        }
-    }
-
-    async getEmotesByID (id, config = { twitch: true }) {
+    async getEmotesByID(id, config = { twitch: true }) {
         if (id == null) {
             throw new Error("You must specify an ID.");
         }
 
         let result = [];
-        
-        if (config.twitch) {
-            result = [...result, ...await this.twitchProvider.getEmotesByID(id)];
-        }
 
         if (config.ffz) {
-            result = [...result, ...await this.ffzProvider.getEmotesByID(id)];
+            result = [...result, ...(await this.ffzProvider.getEmotesByID(id))];
         }
 
         if (config.bttv) {
-            result = [...result, ...await this.bttvProvider.getEmotesByID(id)];
+            result = [
+                ...result,
+                ...(await this.bttvProvider.getEmotesByID(id)),
+            ];
         }
 
         if (config["7tv"]) {
-            let userdata = await this.getUserData({id});
-            result = [...result, ...await this.stvProvider.getEmotesByName(userdata.login)];
+            let userdata = await this.getUserData({ id });
+            result = [
+                ...result,
+                ...(await this.stvProvider.getEmotesByName(userdata.login)),
+            ];
         }
 
         return result;
     }
 
-    async getEmotesByName (username, settings) {
+    async getEmotesByName(username, settings) {
         if (username == null) {
             throw new Error("You must specify an Username.");
         }
 
-        const data = await this.getUserData({username});
+        const data = await this.getUserData({ username });
         if (data == null) {
             throw new Error("Couldn't fetch this user.");
         }
